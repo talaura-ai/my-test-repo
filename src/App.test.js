de0519bf093fea37119c2ba71325a81e27f40d80
talaura-ai <kalim@talaura.ai>
2026-05-19 23:16:36 +0530

    fail test case implemnet

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test('fails intentionally', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // This text does not exist, so test will fail
+  const element = screen.getByText(/this text will fail/i);
+
+  expect(element).toBeInTheDocument();
 });
